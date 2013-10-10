975f7e700593961eac31504a611bccb295ebeeb2
Lorenzo Boccaccia <LORENZOB@ie.ibm.com>
2013-10-10 14:22:01 +0100

    prepared community test for mocking strategy integration

--- test/java/com.ibm.sbt.core.test/test/lib/TestEndpoint.java
+++ test/java/com.ibm.sbt.core.test/test/lib/TestEndpoint.java
@@ -9,6 +9,7 @@ import com.ibm.sbt.services.client.ClientService.Args;
 import com.ibm.sbt.services.client.ClientServicesException;
 import com.ibm.sbt.services.client.Response;
 import com.ibm.sbt.services.endpoints.BasicEndpoint;
+import com.ibm.sbt.services.endpoints.Endpoint;
 
 /**
  * this class bridges the endpoint as required from the API with a more convenient method to handle the
@@ -21,6 +22,7 @@ public abstract class TestEndpoint extends BasicEndpoint {
 
 	TestRuntimeFactory	runtime;
 	TestContext			context;
+	private Endpoint ep;
 
 	@Before
 	public void setUpEndpoint() {
@@ -31,11 +33,16 @@ public abstract class TestEndpoint extends BasicEndpoint {
 		// resources used within the test and to perform assreion over API usage
 		context = (TestContext) Context.init(runtime.getApplicationUnchecked(), null, null);
 
+		
+		//currently only forward to connections
+		this.ep = (Endpoint)context.getBean("connections");
+		
 		// the test class injected into the context is our test server and will serve and check
 		// all the API calls performed within this test class and this only
 		context.addBean("connections", this);
 		context.addBean("smartcloud", this);
 		setUrl(this.getClass().getResource(".").toString());
+		
 	}
 
 	@After
@@ -70,6 +77,9 @@ public abstract class TestEndpoint extends BasicEndpoint {
 		};
 	}
 
-	protected abstract Response testRequest(String method, Args args, Object content)
-			throws ClientServicesException;
+	protected Response testRequest(String method, Args args, Object content) throws ClientServicesException {
+		
+		return ep.xhr(method, args, content);
+		
+	}
 }
