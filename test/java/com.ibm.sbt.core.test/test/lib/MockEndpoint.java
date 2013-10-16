bb7475a7f1132516fb79a9a425d8e7bfee398136
Carlos Manias <carlmani@ie.ibm.com>
2013-10-16 13:13:58 +0100

    Minor improvements

--- test/java/com.ibm.sbt.core.test/test/lib/MockEndpoint.java
+++ test/java/com.ibm.sbt.core.test/test/lib/MockEndpoint.java
@@ -1,3 +1,19 @@
+/*
+ * © Copyright IBM Corp. 2013
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package lib;
 
 import com.ibm.sbt.services.client.ClientService;
@@ -21,13 +37,8 @@ public class MockEndpoint extends BasicEndpoint {
 	
     @Override
 	public ClientService getClientService() throws ClientServicesException {
-    	MockService mockService;
-    	if (endpoint!=null) {
-    		mockService = new MockService(endpoint.getClientService());
-    	} else {
-    		mockService = new MockService(EndpointFactory.getEndpoint(innerEndpoint).getClientService(), mockMode);
-    	}
-    	return mockService;
+    	Endpoint ep = endpoint!=null?endpoint:EndpointFactory.getEndpoint(innerEndpoint);
+    	return new MockService(ep.getClientService(), mockMode);
     }
 
 	public String getInnerEndpoint() {
--- test/java/com.ibm.sbt.core.test/test/lib/MockService.java
+++ test/java/com.ibm.sbt.core.test/test/lib/MockService.java
@@ -1,5 +1,22 @@
+/*
+ * © Copyright IBM Corp. 2013
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package lib;
 
+import com.ibm.commons.util.StringUtil;
 import com.ibm.sbt.services.client.ClientService;
 import com.ibm.sbt.services.client.ClientServicesException;
 import com.ibm.sbt.services.client.Response;
@@ -18,7 +35,7 @@ public class MockService extends ClientService {
     public MockService(ClientService svc, String mockMode) {
     	this.service = svc;
     	this.endpoint = svc.getEndpoint();
-    	this.mode = MockMode.valueOf(mockMode.toUpperCase());
+    	this.mode = StringUtil.isEmpty(mockMode)?MockMode.PASSTHRU:MockMode.valueOf(mockMode.toUpperCase());
     }
 
     @Override
